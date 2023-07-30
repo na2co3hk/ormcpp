9d8238396577742fd2f2e3756ec323b8436028ed
na2co3hk <2515579692@qq.com>
2023-07-30 18:49:35 +0800

    Revert "Revert "添加项目文件。""
    This reverts commit 53102b64469e3a3dbb53dd9cff395f2d340067bb.

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include"ormcpp/ormcpp.h"
+using namespace std;
+
+class Person {
+public:
+	DEFINE_TABLE(Person, id, name, age);
+
+	INTEGER id;
+
+	TEXT name;
+
+	INTEGER age;
+};
+
+int main() {
+	Person p{ 3, "JOJO", 15};
+	Person q{ 1, "Huake", 23 };
+
+	ormcpp<mysql>& pool = ormcpp<mysql>::instance();
+	int max_connection_size = 5;
+	pool.init(max_connection_size, "your ip", "your port", "your database password", "database name");
+	auto con = pool.get();
+	auto t = con->query<std::tuple<int, std::string>>("select id, name from Person order by id");
+	for (auto d : t) {
+		cout << std::get<0>(d) << ' ' << std::get<1>(d) << endl;
+	}
+	return 0;
+}
