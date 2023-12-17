db1474ba4d255dfcf6ea0e52e988f5262542d948
na2co3hk <99656524+na2co3hk@users.noreply.github.com>
2023-12-17 16:48:31 +0800

    Update reflection.hpp

--- reflection.hpp
+++ reflection.hpp
@@ -98,11 +98,129 @@ private:
 
 };
 
+#if __cplusplus >= 201402L
+template<typename T>
+class FixedBasicString {
+private:
+    using CharType = T;
+    CharType const *data;
+    size_t length;
+public:
+    template <size_t N>
+    constexpr FixedBasicString(CharType const (&data)[N]) : data(data), length(N - 1) {}
+    constexpr FixedBasicString(CharType const *data, size_t size): data(data), length(size) {}
+
+    constexpr FixedBasicString(const FixedBasicString&) noexcept = default;
+    constexpr FixedBasicString &operator=(const FixedBasicString&) noexcept = default;
+
+    constexpr size_t getLength() const {
+        return length;
+    }
+
+    constexpr CharType operator[](size_t index) const {
+        return data[index];
+    }
+
+    constexpr bool operator==(const FixedBasicString &other) const {
+        if (length != other.length) {
+            return false;
+        }
+        for (size_t i = 0;i < length; i++) {
+            if (data[i] != other.data[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    constexpr bool operator<(const FixedBasicString &other) const {
+        unsigned i = 0;
+        for (; i < length and i < other.length; i++) {
+            if ((*this)[i] < other[i]) {
+                return true;
+            }
+            if ((*this)[i] > other[i]) {
+                return false;
+            }
+        }
+        return length < other.length;
+    }
+
+    constexpr const CharType* getData() const {
+        return data;
+    }
+
+    constexpr const CharType* begin() const {
+        return data;
+    }
+
+    constexpr const CharType * end() const {
+        return data + length;
+    }
+};
+
+template<typename String>
+constexpr size_t stringHash(const String& value) {
+    size_t d = 5381;
+    for (const auto c : value) {
+        d = d * 33 + static_cast<size_t>(c);
+    }
+    return d;
+}
+
+// https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+// With the lowest bits removed, based on experimental setup.
+template<typename String>
+constexpr size_t stringHash(const String& value, size_t seed) {
+    std::size_t d = (0x811c9dc5 ^ seed) * static_cast<size_t>(0x01000193);
+    for (const auto& c : value)
+        d = (d ^ static_cast<size_t>(c)) * static_cast<size_t>(0x01000193);
+    return d >> 8;
+}
+
+template<typename T>
+struct StringHash {
+    constexpr size_t operator() (T value) const {
+        return stringHash(value);
+    }
+    constexpr size_t operator() (T value, size_t seed) const {
+        return stringHash(value, seed);
+    }
+};
+
+using FixedString = FixedBasicString<char>;
+#endif
+
+#if __cplusplus >= 201402L
+class Object {
+public:
+    std::unordered_map<FixedString, Any, StringHash<FixedString>>metaData;
+};
+#else
 class Object {
 public:
     std::unordered_map<std::string, Any>metaData;
 };
+#endif
 
+#if __cplusplus >= 201402L
+#define EMBER_REFLECTION(className) \
+template<typename T> \
+T getValue(const Ember::FixedString& fieldName) { \
+    return this->*metaData[fieldName].to<T className::*>(); \
+} \
+template<typename T> \
+void setValue(const Ember::FixedString& fieldName, const T& value) { \
+    this->*metaData[fieldName].to<T className::*>() = value; \
+} \
+template<typename T = void, typename... Args> \
+T invokeMethod(const Ember::FixedString& methodName, Args &&... args) { \
+    using Method = T (className::*)(Args...); \
+    auto method = metaData[methodName].to<Method>(); \
+    return (static_cast<className*>(this)->*method)(std::forward<Args>(args)...); \
+} \
+
+#else
 #define EMBER_REFLECTION(className) \
 template<typename T> \
 T getValue(const std::string& fieldName) { \
@@ -118,6 +236,8 @@ T invokeMethod(const std::string& methodName, Args &&... args) { \
     auto method = metaData[methodName].to<Method>(); \
     return (static_cast<className*>(this)->*method)(std::forward<Args>(args)...); \
 } \
+
+#endif
 
 #define EMBER_PROPERTY(className, fieldName) \
 int fieldName##InitMetaData = [this]() -> int { \
